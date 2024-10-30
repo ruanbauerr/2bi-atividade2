2beec8ef740519780d6ea2b1fa6e2889be857248
ruanbauerr <168039811+ruanbauerr@users.noreply.github.com>
2024-10-30 19:06:08 -0300

    Add files via upload
    main UniparCoins

--- Main.java
+++ Main.java
@@ -1,29 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static CarteiraDigital carteira = new CarteiraDigital();
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String comando = "";
+        System.out.println("Bem-vindo à sua Carteira Digital!");
+        int opcao = 0;
 
-        MiniChatbot MiniChatbot = new MiniChatbot();
-
-        System.out.println("Olá! Eu sou o MiniChatbot. Como posso te ajudar hoje?");
-
-        do {
-            System.out.print("Usuário:");
-            comando = scanner.nextLine();
-
-            if (comando.toUpperCase().contains("OI") || comando.contains("OLA")) {
-                System.out.println("Minichat :" + MiniChatbot.dizerOi());
-            } else if (comando.toUpperCase().contains("COMO VOCE ESTA?")) {
-
-                System.out.println("Minichat :" + MiniChatbot.comoVoceEsta());
-            } else if (comando.toUpperCase().contains("QUAL É O SEU NOME?")) {
-
-                System.out.println("Minichat :" + MiniChatbot.qualNome());
+        while (opcao != 4) {
+            opcao = menu();
+            if (opcao == 1) {
+                System.out.print("Digite o valor para adicionar ao saldo: ");
+                double valorAdicionar = Double.parseDouble(scanner.nextLine());
+                carteira.adicionarSaldo(valorAdicionar);
+            } else if (opcao == 2) {
+                System.out.print("Digite o valor do pagamento: ");
+                double valorPagamento = Double.parseDouble(scanner.nextLine());
+                carteira.realizarPagamento(valorPagamento);
+            } else if (opcao == 3) {
+                System.out.printf("Seu saldo atual é: R$ %.2f", carteira.verificarSaldo());
+            } else if (opcao != 4) {
+                System.out.println("Opção inválida. Tente novamente.");
             }
+        }
 
+        System.out.println("Obrigado por usar a sua Carteira Digital!");
+    }
 
-        } while (!comando.contains("sair"));
+    public static int menu() {
+        System.out.println("Escolha uma opção:");
+        System.out.println("1 - Adicionar Saldo");
+        System.out.println("2 - Realizar Pagamento");
+        System.out.println("3 - Verificar Saldo");
+        System.out.println("4 - Sair");
+        System.out.print("Usuário: ");
+        return Integer.parseInt(scanner.nextLine());
     }
 }
